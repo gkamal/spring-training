09a59236847b1533ebf425b5064cb3bf0fbbd630
Kamal Govindraj <kamal@tenxperts.com>
2013-04-26 12:58:55 +0530

    java config

---
+++ src/main/java/tenx/store/config/ApplicaitonConfig.java
@@ -0,0 +1,32 @@
+package tenx.store.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import tenx.store.OrderService;
+import tenx.store.internal.InMemoryProductDao;
+import tenx.store.internal.ProductDao;
+import tenx.store.internal.SimpleOrderService;
+
+@Configuration
+public class ApplicaitonConfig {
+
+//	@Bean
+//	public OrderService orderService2() {
+//		SimpleOrderService orderService = new SimpleOrderService(productDao());
+//		return orderService;
+//	}
+
+	@Bean
+	public OrderService orderService() {
+		SimpleOrderService orderService = new SimpleOrderService(productDao());
+		return orderService;
+	}
+	
+	
+	@Bean
+	public ProductDao productDao() {
+		return new InMemoryProductDao();
+	}
+
+}
